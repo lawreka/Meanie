01192f05fd74db027d9f3b4f38c8aac7bbe00ac4
Kathryn Lawrence <lawr3ka@gmail.com>
2017-12-14 15:01:31 +0100

    Finished the Meanie prototype
    added text boxes and texts
    added angry eyes to penultimate Meanie
    changed meanie color changing function to next
    set up state counts to match text, size, and anger levels

--- meanie.js
+++ meanie.js
@@ -1,9 +1,29 @@
-$("#meanie").on("click", function(){
-  var colors = ["", "lightred", "redder", "red", "medred", "darkred"]
-  var sizes = ["", "lil", "med", "big", "bigger", "biggest"]
+$("#next").on("click", function(){
+  var colors = ["", "redder", "red", "medred", "black"]
+  var sizes = ["", "med", "big", "bigger", "biggest"]
+  var meaniespeak = ["Well what do you want?", "I don't have all day", "Speak, puny human", "You are redundant", "and must be destroyed"]
+  var userspeak = ["Umm", "...", "I'm sorry?", "???", "!!!"]
   var count = $(this).data("count");
   console.log(count);
-  $(this).removeClass(colors[count]).addClass(colors[count+1]);
-  $(this).removeClass(sizes[count]).addClass(sizes[count+1]);
+  if (count < 3){
+    $("#meanie").removeClass(colors[count]).addClass(colors[count+1]);
+    $("#meaniesays").text(meaniespeak[count]);
+    $("#meanie").removeClass(sizes[count]).addClass(sizes[count+1]);
+    $("#usersays").text(userspeak[count]);
+  }
+  else if (count < 4){
+    $("#meanie").removeClass(colors[count]).addClass(colors[count+1]);
+    $("#meaniesays").text(meaniespeak[count]);
+    $("#meanie").removeClass(sizes[count]).addClass(sizes[count+1]);
+    $("#usersays").text(userspeak[count]);
+    $("#angryL").removeClass("hide");
+    $("#angryR").removeClass("hide");
+  }
+  else{
+    $("#meaniesays").text(meaniespeak[count]);
+    $("#usersays").text(userspeak[count]);
+    $("#lilmeanie").addClass("hide");
+    $("#fullface").removeClass("hide");
+  }
   $(this).data("count", count+1);
 });
